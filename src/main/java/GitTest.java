7f4eae70e1606722e5e81deebb38cd9fcf51a359
kang <1289465464@qq.com>
2021-06-11 11:02:24 +0800

    hot-fix first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git-test");
         System.out.println("git-second-commit");
+        System.out.println("hot-fix");
     }
 }
